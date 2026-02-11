f632d0e779a3b44cf917be8e108b6edfb6cf8213
Moneswar <moneswar51@gmail.com>
2026-02-11 10:24:03 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "game_server" {
-  ami           = "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20251212" 
+  ami           = "ami-019715e0d74f695be" 
   instance_type = "t3.small"
   key_name      = "jenkins"
 
-  security_groups = [aws_security_group.game-sg-jenkins.name]
+  security_groups = [aws_security_group.game.name]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -22,8 +22,8 @@ resource "aws_instance" "game_server" {
   }
 }
 
-resource "aws_security_group" "gamenew" {
-  name = "gamenew"
+resource "aws_security_group" "game" {
+  name = "game"
 
   ingress {
     from_port   = 22
